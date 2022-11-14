3086fdf9bab5db84a27cf919ba81e33dfe430d4d
beichen515 <maoss993@newschool.edu>
2022-11-14 03:54:52 -0500

    changeweek5

---
+++ week5/js/shadow.js
@@ -0,0 +1,17 @@
+$("span").each(function(index, element){
+  var animation = TweenMax.to(this, 0.2, {
+    className: '+= superShadow',
+    marginTop: '-10px',
+    marginBottom: '10px',
+    ease: Power1.easeIn,
+    paused:true
+  });
+  element.animation = animation;
+})
+
+
+$('span').hover(function(){
+ this.animation.play()
+}, function(){
+ this.animation.reverse();
+})
